cd88a695cd7d83a5e5c10ccb3fb84e55600de8c6
LordNature <LordNature@protonmail.com>
2018-09-19 02:21:06 +0000

    Fixed pi.txt working directory

--- gala.py
+++ gala.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	with open('app/static/pi.txt', 'r') as f:
+	with open('static/pi.txt', 'r') as f:
 		pi = f.read()
-	#b'up 1 day, 21 hours, 48 minutes\n'
 	uptime = subprocess.check_output('uptime -p', shell=True)
 	uptime = re.findall(r"'(.*?)\\n'", str(uptime))[0]
 	return render_template('home.html', year=time.strftime('%Y'), pi=pi, uptime=uptime)
