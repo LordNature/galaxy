24ca4eb21c6700c51b324da0926795ffe6c62f2d
LordNature <LordNature@protonmail.com>
2018-09-27 03:43:37 +0000

    Kitsu API

--- gala.py
+++ gala.py
@@ -1,6 +1,8 @@
 # Init file for gala
 from flask import Flask, render_template
+from kitsu import *
 import time, subprocess, re
+
 
 app = Flask(__name__)
 
@@ -11,6 +13,12 @@ def home():
 	uptime = subprocess.check_output('uptime -p', shell=True)
 	uptime = re.findall(r"'(.*?)\\n'", str(uptime))[0]
 	return render_template('home.html', year=time.strftime('%Y'), pi=pi, uptime=uptime)
+
+@app.route('/anime')
+def anime():
+	kitsu = fetchUser('Nature')['data'][0]
+	totalAnime = minsToString(kitsu['attributes']['lifeSpentOnAnime'])
+	return render_template('anime.html', kitsu = kitsu, totalAnimeTime = totalAnime)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- kitsu.py
+++ kitsu.py
@@ -1,8 +1,36 @@
-# Kitsu API wrapper by Nature
-import json
-from pprint import pprint
+import requests, json
 
-with open('data.json') as f:
-    data = json.load(f)
+# Fetches Kitsu API
+def fetchUser(username):
+	response = requests.get(
+		'https://kitsu.io/api/edge/users?filter[name]=' + username,
+		headers = {
+			'Content-Type': 'application/vnd.api+json',
+			'Accept': 'application/vnd.api+json'
+		})
+	if response.status_code == 200:
+		return json.loads(response.content.decode('utf-8'))
+	else:
+		return None
 
-pprint(data)
+def minsToString(mins):
+	dateTypes = {
+		'year': 525600, # 24*60*365
+		'month': 43200, # 24*60*30
+		'day': 1440, # 24*60
+		'hour': 60,
+		'minute': 1
+	}
+	result = []
+
+	# I obviously broke something in this.
+	for types in dateTypes:
+		time = mins // dateTypes[types]
+		if time == 0:
+			result.append(time)
+		elif time == 1:
+			result.append(" " + str(time) + " " + types)
+		elif time >= 2:
+			result.append(" " + str(time) + " " + types + "s")
+		mins %= dateTypes[types]
+	return result
