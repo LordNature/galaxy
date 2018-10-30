6e8f05da2db9aef1766022fdad0dd3c495a10640
LordNature <LordNature@protonmail.com>
2018-10-30 02:19:08 +0000

    Removed deprecated functions

--- music.py
+++ music.py
@@ -1,26 +1,6 @@
 # Fetches Youtube API music and displays from text array
 import random
 
-def fetchVideo():
-	with open('static/videos.txt', 'r') as f:
-		music = f.readlines()
-	return random.choice(music)
-
-# Less efficient?
-def fetchPhraseOld():
-	with open('static/phrase.txt', 'r') as f:
-		phrase = f.readlines()
-	return random.choice(phrase)
-
-# More efficient?
-def fetchPhrase():
-	with open('static/phrase.txt', 'r') as f:
-		for count, line in enumerate(f, 2):
-			if random.randrange(count): continue
-			phrase = line
-		return phrase
-
-# simplify both into one function
 def fetchLine(fileName):
 	with open(fileName, 'r') as f:
 		# Initiaizes vars in rare case of missing declaration on return
