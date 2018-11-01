56f7326e1296c228637c14984fa5079e5e3deeb7
LordNature <LordNature@protonmail.com>
2018-11-01 01:01:26 +0000

    Rollbacked fetchLine function and added caching instead for even randomization; thanks jumbo

--- music.py
+++ music.py
@@ -1,12 +1,10 @@
 # Fetches Youtube API music and displays from text array
 import random
 
-def fetchLine(fileName):
-	with open(fileName, 'r') as f:
-		# Initiaizes vars in rare case of missing declaration on return
-		line = next(f)
-		randLine = line
-		for count, line in enumerate(f, 1):
-			if random.randrange(count): continue
-			randLine = line
-		return randLine
+file_map = {}
+def fetchLine(filename):
+    global file_map
+    if not (filename in file_map.keys()):
+        with open(filename, 'r') as f:
+            file_map[filename] = f.readlines()
+    return random.choice(file_map[filename])
